9a1948a221eed985650dd687c5da7ba1e8df5589
Arnanz <pashamaulana14@gmail.com>
2023-11-01 21:34:12 +0700

    add

--- WRI_per1.java
+++ WRI_per1.java
@@ -2,5 +2,7 @@ public class WRI_per1 {
         public static void main(String[] args) {
             System.out.println("Hello world");
             System.out.println("Good Morning");
+            System.out.println("Good Afternoon");
+            System.out.println("pasha");
         }
     }
